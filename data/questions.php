16565d1ed50ef8fd03b9bf23d7fee73f1a4d0e32
emma-fishrod <emma.brackenridge@fishrod.co.uk>
2012-05-21 14:46:23 +0100

    Emma Update
    http://www.pcrweb.co.uk/vacancies/results.asp?keywords=&Char_3=&newsearc
    h=Search&AndOr=and

--- data/questions.php
+++ data/questions.php
@@ -1,26 +1,89 @@
 <?php
 
-/* How Cool Are You? */
+/* Do you code? */
 $question = new Question();
-$question->description = "How cool are you?";
+$question->description = "Do you code?";
 $answers = array(
-	new Answer('0%', 0),
-	new Answer('30%', 30),
-	new Answer('70%', 70)
+	new Answer('No', -600),
+	new Answer('I know a bit, beginner style', 25),
+	new Answer('I\'m fairly good at coding', 50),
+	new Answer('I\'ll Java your Script', 75),
+	new Answer('I code like Hugh Jackman in Swordfish', 100)
 );
 
 $question->answers = $answers;
 
 $quiz->addQuestion($question);
 
-/* How Cool Are You? */
+/* What would your coding montage be played to? */
 $question = new Question();
-$question->description = "How cool are you?";
+$question->description = "What would your coding montage be played to?";
 $answers = array(
-	new Answer('0%', 0),
-	new Answer('30%', 30),
-	new Answer('This is an answer', 60),
-	new Answer('This is a second answer', 10)
+	new Answer('Justin Bieber', 0),
+	new Answer('A classical piece, maybe by Chopin', 25),
+	new Answer('The top 40 chart', 50),
+	new Answer('Tu-Pac til I die', 75),
+	new Answer('Dubstep baby!', 100)
+);
+
+$question->answers = $answers;
+
+$quiz->addQuestion($question);
+
+/* How would you describe your body? */
+$question = new Question();
+$question->description = "How would you describe your body?";
+$answers = array(
+	new Answer('Average at best', 0),
+	new Answer('Bit of a beer belly', 25),
+	new Answer('I try to look after my body', 50),
+	new Answer('I work out, but the tan is natural', 75),
+	new Answer('Ripped with a wicked tan!', 100)
+);
+
+$question->answers = $answers;
+
+$quiz->addQuestion($question);
+
+/* Do you have any coding eyewear? */
+$question = new Question();
+$question->description = "Do you have any coding eyewear?";
+$answers = array(
+	new Answer('No', 0),
+	new Answer('Goggles', 25),
+	new Answer('An eye patch', 50),
+	new Answer('Ray Bans', 75),
+	new Answer('Prada sunglasses', 100)
+);
+
+$question->answers = $answers;
+
+$quiz->addQuestion($question);
+
+/* What do you eat while coding? */
+$question = new Question();
+$question->description = "What do you eat while coding?";
+$answers = array(
+	new Answer('My mum makes me a packed lunch', 0),
+	new Answer('I am so focused on coding I ', 25),
+	new Answer('Snacks', 50),
+	new Answer('Meat, meat, and meat - low carbs', 75),
+	new Answer('A balanced diet of protein, carbs and veg', 100)
+);
+
+$question->answers = $answers;
+
+$quiz->addQuestion($question);
+
+/* How often do you party? */
+$question = new Question();
+$question->description = "How often do you party?";
+$answers = array(
+	new Answer('Never been invited out', 0),
+	new Answer('Only on special occasions', 25),
+	new Answer('Every weekend', 50),
+	new Answer('Occasionally on a school night', 75),
+	new Answer('Every night\'s a party!', 100)
 );
 
 $question->answers = $answers;
--- data/rewards.php
+++ data/rewards.php
@@ -2,11 +2,11 @@
 	
 	/* Create the rewards */
 	$rewards = array(
-		new Reward(0, 'Your rubbish', 'img/rubbish.png'),
-		new Reward(20, 'You\'re not too bad', 'img/not-bad.png'),
-		new Reward(30, 'You\'re alright', 'img/alright.png'),
-		new Reward(80, 'You\'re not quite awesone!', 'img/awesome.png'),
-		new Reward(100, 'You\'re fucking awesone!', 'img/awesome.png')
+		new Reward(0, 'You can\'t even code, you\'re no developer!', 'img/rubbish.png'),
+		new Reward(20, 'You are a true programmer, none of this frat boy rubbish - we salute you!', 'img/not-bad.png'),
+		new Reward(30, 'Dude, your cool, but you\'re no brogrammer', 'img/alright.png'),
+		new Reward(80, 'Complete geek chic, but not quite a brogrammer! ', 'img/awesome.png'),
+		new Reward(100, 'Totes brogrammer stuff there dude', 'img/awesome.png')
 	);
 	
 	$quiz->rewards = $rewards;
